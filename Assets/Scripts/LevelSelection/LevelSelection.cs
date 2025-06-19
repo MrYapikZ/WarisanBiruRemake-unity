bc9e1c2bfd05adff2bcbb5cefa59cdd121a69aff
MrYapikZ <yafialhaidar18@gmail.com>
2025-06-19 10:46:54 +0700

    feat(LevelSelection): Unlock next level

--- Assets/Scripts/LevelSelection/LevelSelection.cs
+++ Assets/Scripts/LevelSelection/LevelSelection.cs
@@ -1,3 +1,4 @@
+using ExpiProject.GameManager.SO;
 using ExpiProject.Others;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,19 +8,27 @@ namespace ExpiProject.LevelSelection
 {
     public class LevelSelection : MonoBehaviour
     {
+        [SerializeField] private ScoreDataScriptableObject scoreData;
         [SerializeField] private GameObject levelButtonContainer;
         [SerializeField] private Button backButton;
 
         private void Start()
         {
             Transition.instance.FadeOut();
+
             for (var i = 0; i < levelButtonContainer.transform.childCount; i++)
             {
                 var index = i;
                 levelButtonContainer.transform.GetChild(index).GetComponent<Button>().onClick.AddListener(() =>
-                    Transition.instance.FadeIn(() => SceneManager.LoadScene("Level" +
-                                                                            (index + 1))));
-                levelButtonContainer.transform.GetChild(index).GetComponent<Button>().interactable = false;
+                    Transition.instance.FadeIn(() => SceneManager.LoadScene("Level" + (index + 1))));
+
+                if (levelButtonContainer.transform.childCount > index + 1)
+                {
+                    if (scoreData.lvlScores[i].isGameFinished)
+                        levelButtonContainer.transform.GetChild(index + 1).GetComponent<Button>().interactable = true;
+                    else
+                        levelButtonContainer.transform.GetChild(index + 1).GetComponent<Button>().interactable = false;
+                }
             }
 
             levelButtonContainer.transform.GetChild(0).GetComponent<Button>().interactable = true;
